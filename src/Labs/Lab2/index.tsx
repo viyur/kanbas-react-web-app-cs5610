6a4aa55f62ba16b1c506eea5fa92179b42c1c951
Julie Yang <vickyu.1994@gmail.com>
2024-10-02 01:21:18 -0400

    lab2 done

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -8,10 +8,19 @@ import Corners from "./Corners";
 import Dimensions from "./Dimensions";
 import Positions from "./Positions";
 import Zindex from "./Zindex";
+import Float from "./Float";
+import GridLayout from "./GridLayout";
+import Flex from "./Flex";
+import BootstrapGrids from "./BootstrapGrids";
+import ScreenSizeLabel from "./ScreenSizeLabel";
+import BootstrapTables from "./BootstrapTables";
+import BootstrapLists from "./BootstrapLists";
+import BootstrapForms from "./BootstrapForms";
+import BootstrapNavigation from "./BootstrapNavigation";
 
 export default function Lab2() {
     return (
-        <div id="wd-lab2">
+        <div id="wd-lab2" className="container">
             <h2>Lab 2 - Cascading Style Sheets</h2>
             <h3>Styling with the STYLE attribute</h3>
             <p>
@@ -72,6 +81,15 @@ export default function Lab2() {
             <Dimensions />
             <Positions />
             <Zindex />
+            <Float />
+            <GridLayout />
+            <Flex />
+            <BootstrapGrids />
+            <ScreenSizeLabel />
+            <BootstrapTables />
+            <BootstrapLists />
+            <BootstrapForms />
+            <BootstrapNavigation />
 
 
 
