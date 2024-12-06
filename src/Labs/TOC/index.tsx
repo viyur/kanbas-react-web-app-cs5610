ad04a46124bbb2df3a3f4472c54b2cfedf41b822
Julie Yang <vickyu.1994@gmail.com>
2024-12-05 19:30:29 -0500

    update link to render server

--- src/Labs/TOC/index.tsx
+++ src/Labs/TOC/index.tsx
@@ -2,22 +2,93 @@ import { Link } from "react-router-dom";
 import { useLocation } from "react-router";
 
 export default function TOC() {
-    const { pathname } = useLocation();
-    return (
-        <ul className="nav nav-pills">
-            <li className="nav-item"><Link id="wd-a" className="nav-link" to="/Labs">Labs</Link></li>
+  const { pathname } = useLocation();
+  return (
+    <ul className="nav nav-pills">
+      <li className="nav-item">
+        <Link id="wd-a" className="nav-link" to="/Labs">
+          Labs
+        </Link>
+      </li>
 
-            <li className="nav-item"><Link id="wd-a1" className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`} to="/Labs/Lab1">Lab 1</Link></li>
-            <li className="nav-item"><Link id="wd-a2" className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`} to="/Labs/Lab2">Lab 2</Link></li>
-            <li className="nav-item"><Link id="wd-a3" className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`} to="/Labs/Lab3">Lab 3</Link></li>
-            <li className="nav-item"><Link id="wd-a4" className={`nav-link ${pathname.includes("Lab4") ? "active" : ""}`} to="/Labs/Lab4">Lab 4</Link></li>
-            <li className="nav-item"><Link id="wd-a5" className={`nav-link ${pathname.includes("Lab5") ? "active" : ""}`} to="/Labs/Lab5">Lab 5</Link></li>
+      <li className="nav-item">
+        <Link
+          id="wd-a1"
+          className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}
+          to="/Labs/Lab1"
+        >
+          Lab 1
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          id="wd-a2"
+          className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}
+          to="/Labs/Lab2"
+        >
+          Lab 2
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          id="wd-a3"
+          className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}
+          to="/Labs/Lab3"
+        >
+          Lab 3
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          id="wd-a4"
+          className={`nav-link ${pathname.includes("Lab4") ? "active" : ""}`}
+          to="/Labs/Lab4"
+        >
+          Lab 4
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          id="wd-a5"
+          className={`nav-link ${pathname.includes("Lab5") ? "active" : ""}`}
+          to="/Labs/Lab5"
+        >
+          Lab 5
+        </Link>
+      </li>
 
-            <li className="nav-item"><Link className="nav-link" to="/Kanbas">Kanbas</Link></li>
-            <li className="nav-item"><Link className="nav-link" id="wd-github" to="https://github.com/viyur/kanbas-react-web-app-cs5610">My Github Link</Link></li>
-            <li className="nav-item"><Link className="nav-link" id="wd-github-server" to="https://github.com/viyur/kanbas-node-server-app">Node server code Github Link</Link></li>
-            <li className="nav-item"><Link className="nav-link" id="wd-render-server" to="https://kanbas-node-server-app-ct2s.onrender.com">Render Server Root</Link></li>
-
-        </ul>
-    )
+      <li className="nav-item">
+        <Link className="nav-link" to="/Kanbas">
+          Kanbas
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          className="nav-link"
+          id="wd-github"
+          to="https://github.com/viyur/kanbas-react-web-app-cs5610"
+        >
+          My Github Link
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          className="nav-link"
+          id="wd-github-server"
+          to="https://github.com/viyur/kanbas-node-server-app"
+        >
+          Node server code Github Link
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          className="nav-link"
+          id="wd-render-server"
+          to="https://kanbas-node-server-app-a6-drwu.onrender.com"
+        >
+          Render Server Root
+        </Link>
+      </li>
+    </ul>
+  );
 }
